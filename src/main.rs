56713dc41c08eb2fc4143902b4cef83ed74b23d0
Ajaz Ur Rehman <ajazurrehman4@gmail.com>
2025-06-07 12:54:23 +0530

    Setup egui

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use eframe::egui;
+
+fn main() -> eframe::Result {
+    let app_name = "NetCat Payload Injector";
+
+    let options = eframe::NativeOptions {
+        ..Default::default()
+    };
+
+    eframe::run_simple_native(app_name, options, update_fun)
+}
+
+fn update_fun(ctx: &egui::Context, _frame: &mut eframe::Frame) {
+    egui::CentralPanel::default().show(ctx, |ui| {
+        ui.heading("Hello World");
+    });
 }
